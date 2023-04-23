16aa1d83e28297ea11f166389b0d76b912ffc913
Amirhossein Movahedi <movahedisefat03@gmail.com>
2023-04-23 15:07:58 +0330

    adding a function to convert a FA to a single final state FA

--- phase4.py
+++ phase4.py
@@ -16,3 +16,20 @@ def convertDFAtoNFA(data: dict) -> None:
 
     return None
 
+def makeSingleFinalState(data: dict) -> None:
+    states = data['states'][2:-2].split("','")
+    states.sort(key=lambda x: int(x[1:]))
+    new_final_state = "q" + str(int(states[-1][1:]) + 1)
+    final_states = data['final_states'][2:-2].split("','")
+    if not is_NFA(data):
+        convertDFAtoNFA(data)
+    if len(final_states) > 1:
+        data["states"] = data["states"][:-1] + ",'" + new_final_state + "'}"
+        data['final_states'] = "{'" + str(new_final_state) + "'}"
+        for i in final_states:
+            if data["transitions"][i].get("", False):
+                data["transitions"][i][""] = data["transitions"][i][-1] + ",'" + new_final_state + "'}"
+            else:
+                data["transitions"][i][""] = "{'" + new_final_state + "'}"
+        data['transitions'][new_final_state] = {}
+
